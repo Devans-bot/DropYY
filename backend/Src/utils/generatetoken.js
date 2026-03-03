e8910315243d1dd6c6c002f78dd13a6e613519a9
Divyansh <divyansh@Divyanshs-MacBook-Pro.local>
2026-03-03 16:06:06 +0530

    change

--- backend/Src/utils/generatetoken.js
+++ backend/Src/utils/generatetoken.js
@@ -1,15 +1,15 @@
-import jwt from "jsonwebtoken"
+import jwt from "jsonwebtoken";
 
-const genereateToken=(id,res)=>{
-    const token=jwt.sign({id},process.env.JWT_SEC,{
-        expiresIn:"7d"
-    })
+const generateToken = (id, res) => {
+  const token = jwt.sign({ id }, process.env.JWT_SEC, {
+    expiresIn: "7d",
+  });
 
-    res.cookie("token", token, {
-  httpOnly: true,
-  sameSite: "lax",   // OR "none" with secure
-  secure: false,    // true only if HTTPS
-});
-}
+  res.cookie("token", token, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production", 
+    sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+  });
+};
 
-export default genereateToken
+export default generateToken;
