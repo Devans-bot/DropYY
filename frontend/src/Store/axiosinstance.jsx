32b0c2288fbca1c6a7ee037ab015dd9b3f9555f5
Divyansh <divyansh@Divyanshs-MacBook-Pro.local>
2026-03-03 16:02:04 +0530

    change

--- frontend/src/Store/axiosinstance.jsx
+++ frontend/src/Store/axiosinstance.jsx
@@ -2,6 +2,6 @@ import React from "react";
 import axios from 'axios'
 
 export const axiosinstance=axios.create({
-    baseURL:"http://localhost:5002/api",
+    baseURL:"/api",
     withCredentials:true
 })
