a62efa6cf0405c8304b552b052b460767beaa8c8
Divyansh <divyansh@Divyanshs-MacBook-Pro.local>
2026-03-03 15:36:54 +0530

    change

--- backend/index.js
+++ backend/index.js
@@ -27,9 +27,9 @@ if (process.env.NODE_ENV === "production") {
   const __dirname = path.resolve();
 
 app.use(express.static(path.join(__dirname, "../frontend/dist")));
- app.get("/*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
-  });
+ app.use((req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+});
 }
 
 app.listen(PORT,async()=>{
